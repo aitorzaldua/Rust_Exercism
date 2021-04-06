ea10b59f5f44423668594dba6dfb449fae93b038
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-04-06 07:55:47 +0200

    april 6 in the morning

--- checkifprime/src/main.rs
+++ checkifprime/src/main.rs
@@ -1,20 +1,43 @@
+use std::io;
+
+
 fn main() {
 
-    let if_prime = 15;
+    println! ("Este programa adivina si un número es primo o no");
+    println! ("!Además, te va a ofrecer un montón de datos interesantes!");
+    println! ("Comencemos, inserta un número entre 1 y... mmm ¡4000000000!");
+
+    let mut if_prime = String::new();
+    let mut counter_no = 0;
+    //let mut counter_yes = 0;
+
+    io::stdin()
+            .read_line(&mut if_prime)
+            .expect("Failed to read line");
+
+    let if_prime: u32 = if_prime.trim().parse().expect("Please type a number!");
+
 
     for divider1 in 2..if_prime {
 
-        let mut counter = 0;
-
         let resto = if_prime % divider1;
 
-        println! ("{} % {} = {}",if_prime, divider1, resto);
+        //println! ("{} % {} = {}",if_prime, divider1, resto);
 
         match resto {
-            0 => counter +=1,
-            _ => println!("primo"),
+            0 => counter_no +=1,
+            _ => continue,
         }
 
     }
+
+    if counter_no != 0 {
+        println! ("El número {} NO es un número primo", if_prime);
+    }
+    else {
+        println! ("El número {} SI es un número primo", if_prime);
+    }
+
+    //println!("counter_no = {}, counter_yes = {}", counter_no, counter_yes);
 
 }
--- checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.d
+++ checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.d
@@ -1,5 +1,5 @@
-/mnt/c/users/aitor/Exercism/rust/checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.rmeta: src/main.rs
+/mnt/c/Users/aitor/Exercism/rust/checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.rmeta: src/main.rs
 
-/mnt/c/users/aitor/Exercism/rust/checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.d: src/main.rs
+/mnt/c/Users/aitor/Exercism/rust/checkifprime/target/rls/debug/deps/checkifprime-d63595de4d747a04.d: src/main.rs
 
 src/main.rs:
